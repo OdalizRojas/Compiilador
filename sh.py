f7e26c43002b3327d62e11bfca05044c5b0b02e6
Nicolas Panozo <nicopanozoc@gmail.com>
2022-06-28 11:29:03 -0400

    proyecto completo falta editar detalles

--- sh.py
+++ sh.py
@@ -1,8 +1,14 @@
 import esperanto
 
 while True:
-    text = input('esperanto > ')
-    result, error = esperanto.run('<stdin>', text)
+	text = input('esperanto > ')
+	if text.strip() == "": continue
+	result, error = esperanto.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
